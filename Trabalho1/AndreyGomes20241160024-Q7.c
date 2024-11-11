ef596aefaa647990c5a41e25fd057ee514c3bf6e
Andrey Gomes <andreygdsn@gmail.com>
2024-11-10 21:54:26 -0300

    q7 base

---
+++ Trabalho1/AndreyGomes20241160024-Q7.c
@@ -0,0 +1,18 @@
+/*
+7) Fazer um programa que implementa o jogo da velha. 
+a) O   programa   deve   sempre   mostrar   o   tabuleiro   do   jogo   da   velha,   com   o   jogador   que   deve
+jogar, da seguinte forma: 
+    1    2    3
+A     |     |
+  ----+-----+----
+B     |     |
+  ----+-----+----
+C     |     |
+
+Jogador 2, informe sua jogada: 
+b) Cada jogador (jogador 1 e jogador 2) terá sua vez de jogar. O jogador um tem a marca “X” e o
+jogador 2 a marca “0”. 
+c) O jogador deve informar a célula de interesse: ex: B3. O programa deve verificar se é uma
+célula válida 
+d) O programa deve informar qual foi o ganhador, ou se não houve ganhador
+*/
